eb34d16e7315119b379145675e4cf083ee1a28c5
leesieun <sieun_lee@tmax.co.kr>
2022-12-22 17:37:50 +0900

    add(app):라우트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
+import { createTheme, ThemeProvider } from "@mui/material";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import MainPage from "./components/MainPage";
+import MainPage from "./components/GeneratePage";
 import WelcomePage from "./components/WelcomePage";
 
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      fontFamily: "MonoplexKR-Regular,Segoe UI, Roboto, sans-serif ",
+      textTransform: "none",
+    },
+  },
+});
 function App() {
   // return <MainPage />;
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<WelcomePage />}></Route>
-        <Route path="/generate-redis/*" element={<MainPage />}></Route>
-        {/* <Route path="*" element={<NotFound />}></Route> */}
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<WelcomePage />}></Route>
+          <Route path="/generate-redis/*" element={<MainPage />}></Route>
+          {/* <Route path="*" element={<NotFound />}></Route> */}
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
